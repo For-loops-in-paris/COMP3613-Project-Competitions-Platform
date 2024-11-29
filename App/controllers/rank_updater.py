2343045d5221d9f621f2408ae95d908e5ed81715
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-11-29 16:57:16 -0400

    push to branch

--- App/controllers/rank_updater.py
+++ App/controllers/rank_updater.py
@@ -5,3 +5,4 @@ from App.controllers import update_rankings,update_ratings
 def update_leaderboard(mod_name,comp_name):
     if update_ratings(mod_name, comp_name):
         update_rankings()
+        
