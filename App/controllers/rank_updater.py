bd0c968f42892f69fa5e5fc57878840d89c268de
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-11-29 16:51:27 -0400

    Added rank_updater controller, applying function calls in the relevant class

---
+++ App/controllers/rank_updater.py
@@ -0,0 +1,7 @@
+from App.database import db
+from App.models import  RankUpdater
+from App.controllers import update_rankings,update_ratings
+
+def update_leaderboard(mod_name,comp_name):
+    if update_ratings(mod_name, comp_name):
+        update_rankings()
--- App/views/competition.py
+++ App/views/competition.py
@@ -174,8 +174,9 @@ def confirm_results(comp_name):
     
     competition = get_competition_by_name(comp_name)
 
-    if update_ratings(moderator.username, competition.name):
-        update_rankings()
+    update_leaderboard(moderator.username, competition.name)
+    
+    
 
     leaderboard = display_competition_results(comp_name)
 
