2de2efa77dd087c1ed7d893779bb01a6e9fb4b26
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-12-03 21:23:31 -0400

    Updated init route for deployed application to work

--- App/views/index.py
+++ App/views/index.py
@@ -22,86 +22,13 @@ def leaderboard_page(leaderboard_id):
 
 @index_views.route('/init', methods=['GET'])
 def init():
-    """
     db.drop_all()
     db.create_all()
-    create_student('bob', 'bobpass')
-    create_student('alice', 'alicepass')
-    create_student('charlie', 'charliepass')
-    create_student('david', 'davidpass')
-    create_admin('admin', 'adminpass',1)
-    create_host('admin2', 'adminpass2',2)
-    create_host('admin3', 'adminpass3',3)
-    create_competition('RunTime', 1)
-    join_comp('admin2', 'RunTime')
-    register_student('bob', 'RunTime')
-    register_student('alice', 'RunTime')
-    register_student('charlie', 'RunTime')
-    register_student('david', 'RunTime')
-    create_competition('RunTime2', 1)
-    add_results('admin2', 'bob', "RunTime",5)
-    add_results('admin2', 'alice', "RunTime",10)
-    add_results('admin2', 'charlie', "RunTime",15)
-    create_competition('RunTime3', 1)
-    join_comp('admin3', 'RunTime3')
-    register_student('bob', 'RunTime3')
-    register_student('alice', 'RunTime3')
-    register_student('charlie', 'RunTime3')
-    add_results('admin3', 'bob', "RunTime3",5)
-    add_results('admin3', 'alice', "RunTime3",10)
-    add_results('admin3', 'charlie', "RunTime3",15)
-    """
-    db.drop_all()
-    db.create_all()
-    """
-    stud1 = create_student('stud1', 'stud1pass')
-    stud2 = create_student('stud2', 'stud2pass')
-    stud3 = create_student('stud3', 'stud3pass')
-    stud4 = create_student('stud4', 'stud4pass')
-    stud5 = create_student('stud5', 'stud5pass')
-    stud6 = create_student('stud6', 'stud6pass')
-    stud7 = create_student('stud7', 'stud7pass')
-    stud8 = create_student('stud8', 'stud8pass')
-    stud9 = create_student('stud9', 'stud9pass')
-    stud10 = create_student('stud10', 'stud10pass')
-    mod1 = create_moderator('mod1', 'mod1pass')
-    mod2 = create_moderator('mod2', 'mod2pass')
-    mod3 = create_moderator('mod3', 'mod3pass')
-    comp1 = create_competition('mod1', 'comp1', '09-02-2024', 'CSL', 1, 25)
-    comp2 = create_competition('mod2', 'comp2', '09-02-2024', 'CSL', 2, 20)
-    mod = add_mod('mod1', 'comp1', 'mod3')
 
-    students = ["stud1", "stud2", "stud3"]
-    add_team('mod1', 'comp1', "A", students)
-    add_results('mod1', 'comp1', "A", 16)
-    
-    students = ["stud4", "stud5", "stud6"]
-    add_team('mod1', 'comp1', "B", students)
-    add_results('mod1', 'comp1', "B", 15)
-
-    students = ["stud7", "stud8", "stud9"]
-    add_team('mod1', 'comp1', "C", students)
-    add_results('mod1', 'comp1', "C", 12)
-
-    students = ["stud10", "stud4", "stud7"]
-    add_team('mod2', 'comp2', "A", students)
-    add_results('mod2', 'comp2', "A", 10)
-    
-    students = ["stud2", "stud5", "stud8"]
-    add_team('mod2', 'comp2', "B", students)
-    add_results('mod2', 'comp2', "B", 15)
-
-    students = ["stud3", "stud6", "stud9"]
-    add_team('mod2', 'comp2', "C", students)
-    add_results('mod2', 'comp2', "C", 12)
-
-    update_ratings('mod1', 'comp1')
-    update_rankings()
-
-    update_ratings('mod2', 'comp2')
-    update_rankings()
-    """
-
+    #Create Rank Updater
+    db.session.add(RankUpdater())
+    db.session.commit()
+    print(RankUpdater.query.get(1))
     #creates students
     with open("students.csv") as student_file:
         reader = csv.DictReader(student_file)
@@ -150,27 +77,19 @@ def init():
 
         for competition in reader:
             if competition['comp_name'] != 'TopCoder':
-                update_ratings(competition['mod_name'], competition['comp_name'])
-                update_rankings()
+                update_leaderboard(competition['mod_name'], competition['comp_name'])
+                # update_ratings(competition['mod_name'], competition['comp_name'])
+                # update_rankings()
             #db.session.add(comp)
         #db.session.commit()
     
     competitions_file.close()
 
-    return render_template('leaderboard.html', leaderboard=display_rankings(), user=current_user)
-    """
-@index_views.route('/health', methods=['GET'])
-def health_check():
-    return jsonify({'status':'healthy'})
+    
+ 
 
-@index_views.route('/healthcheck', methods=['GET'])
-def health():
-    return jsonify({'status':'healthy'})
+    return redirect(url_for('index_views.leaderboard_page',leaderboard_id=0))
 
-#@index_views.route('/Student_Profile/<int:user_id>')
-#def Student_Profile(user_id):
- #   return render_template('Student_Profile.html', user_id=user_id)
-"""
 
 @index_views.route('/profile')
 def profile():
