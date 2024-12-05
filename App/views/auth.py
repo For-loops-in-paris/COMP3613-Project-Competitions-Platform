1d0834bfa9a6825a5f9d4fa372bea35b2eca1db9
Mark1Lessey <mark.lessey1@my.uwi.edu>
2024-12-04 20:58:52 -0400

    ALL unnecessary comments gone!

--- App/views/auth.py
+++ App/views/auth.py
@@ -11,70 +11,6 @@ from App.controllers import *
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
 
-'''
-Page/Action Routes
-'''
-
-"""
-@auth_views.route('/users', methods=['GET'])
-def get_user_page():
-    users = get_all_users()
-    return render_template('users.html', users=users)
-
-
-@auth_views.route('/identify', methods=['GET'])
-@login_required
-def identify_page():
-    return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
-
-
-@auth_views.route('/login', methods=['POST'])
-def login_action():
-    data = request.form
-    user = login(data['username'], data['password'])
-    if user:
-        login_user(user)
-        return 'user logged in!'
-    return 'bad username or password given', 401 """
-"""
-@auth_views.route('/logout', methods=['GET'])
-@login_required
-def logout_action():
-    data = request.form
-    user = login(data['username'], data['password'])
-    logout_user()
-    return 'logged out!'
-"""
-'''
-API Routes
-'''
-"""
-@auth_views.route('/api/users', methods=['GET'])
-def get_users_action():
-    users = get_all_users_json()
-    return jsonify(users)
-
-@auth_views.route('/api/users', methods=['POST'])
-def create_user_endpoint():
-    data = request.json
-    response = create_user(data['username'], data['password'])
-    if response:
-        return jsonify({'message': f"user created"}), 201
-    return jsonify(error='error creating user'), 500
-
-@auth_views.route('/api/login', methods=['POST'])
-def user_login_api():
-  data = request.json
-  token = jwt_authenticate(data['username'], data['password'])
-  if not token:
-    return jsonify(error='bad username or password given'), 401
-  return jsonify(access_token=token)
-
-@auth_views.route('/api/identify', methods=['GET'])
-@jwt_required()
-def identify_user_action():
-    return jsonify({'message': f"username: {jwt_current_user.username}, id : {jwt_current_user.id}"})
-"""
 
 @auth_views.route('/login', methods=['GET', 'POST'])
 def login():
@@ -120,10 +56,10 @@ def signup():
         student = create_student(request.form['username'], request.form['password'])
         
         if student:
-            #flash('Username not available!', category="error")
-            #return jsonify({'message': 'Username already used!'})
+            flash('Username not available!', category="error")
+
             if request.form['username'] == student.username:
-            #flash('Account created successfully!', category="success")
+                flash('Account created successfully!', category="success")
                 login_user(student)
                 session['user_type'] = 'student'
                 return redirect(url_for('index_views.leaderboard_page',leaderboard_id=0))
